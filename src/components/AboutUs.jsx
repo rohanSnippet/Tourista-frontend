65fb583d20fb6be37b5d9b59e796d250c4131390
Rohan More <rohan110620@gmail.com>
2024-07-23 19:08:25 +0530

    changes

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { FaGithubAlt, FaEnvelope } from "react-icons/fa";
+import { FaGithubAlt, FaLinkedin } from "react-icons/fa";
 
 const AboutUs = () => {
   return (
-    <div className="bg-gray-100 mt-14 space-y-3">
-      <div className="max-w-4xl mx-auto bg-white shadow-md rounded-lg p-6">
+    <div className="bg-gradient-to-b from-gray-300 via-gray-300/80 to-gray-300 mt-14 space-y-3">
+      <div className="max-w-4xl mx-auto bg-slate-300 shadow-md rounded-lg p-6">
         <h1 className="text-3xl font-bold text-center mb-6">About Us</h1>
 
-        <section className="mb-6">
+        <section className="mb-6 p-4 bg-gradient-to-br from-slate-100 via-slate-50 to-slate-50 border-gray-200 border-b-2 shadow-sm rounded-lg shadow-slate-300">
           <h2 className="text-2xl font-semibold mb-2">Introduction</h2>
           <p className="text-gray-700 mb-2">
             The project aims to develop a dynamic and user-friendly web
@@ -27,7 +27,7 @@ const AboutUs = () => {
           </p>
         </section>
 
-        <section className="mb-6">
+        <section className="mb-6 p-4 bg-gradient-to-br from-slate-100 via-slate-50 to-slate-50 border-gray-200 border-b-2 rounded-lg shadow-sm shadow-slate-300">
           <h2 className="text-2xl font-semibold mb-2">Objective</h2>
           <ul className="list-disc list-inside text-gray-700">
             <li>
@@ -53,7 +53,7 @@ const AboutUs = () => {
           </ul>
         </section>
 
-        <section className="mb-6">
+        <section className="mb-6 p-4 bg-gradient-to-br from-slate-100 via-slate-50 to-slate-50 border-gray-200 border-b-2 rounded-lg shadow-sm shadow-slate-300">
           <h2 className="text-2xl font-semibold mb-2">Scope</h2>
           <ul className="list-disc list-inside text-gray-700">
             <li>
@@ -83,10 +83,10 @@ const AboutUs = () => {
           </ul>
         </section>
 
-        <div className="bg-gray-100 p-8">
+        <div className="bg-gray-100 rounded-xl p-8">
           <div className="max-w-4xl mx-auto">
             <div className="flex flex-wrap justify-center gap-8">
-              <div className="bg-white shadow-md rounded-lg p-6 w-80">
+              <div className="bg-white shadow-md rounded-lg p-6 w-80 border-gray-100 border-b-2 shadow-slate-400 hover:shadow-2xl hover:shadow-pink-400 hover:bg-purple-100">
                 <h2 className="text-2xl font-semibold mb-4">Rohan More</h2>
                 <p className="text-gray-700 mb-4">
                   This project was developed by Rohan More.
@@ -96,19 +96,21 @@ const AboutUs = () => {
                     href="https://github.com/rohanSnippet"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-slate-800 text-2xl"
+                    className="text-slate-800 text-2xl shadow-lg shadow-gray-600 hover:shadow-gray-900 hover:shadow-xl rounded-xl hover:-translate-y-2 duration-200 transition-all"
                   >
                     <FaGithubAlt />
                   </a>
                   <a
-                    href="mailto:rohan@example.com"
-                    className="text-slate-800 text-2xl"
+                    href="https://www.linkedin.com/in/rohan-more-80363221b/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-slate-800 text-2xl shadow-lg shadow-gray-600 hover:-translate-y-2 duration-200 transition-all hover:shadow-gray-900 hover:shadow-xl"
                   >
-                    <FaEnvelope />
+                    <FaLinkedin />
                   </a>
                 </div>
               </div>
-              <div className="bg-white shadow-md rounded-lg p-6 w-80">
+              <div className="bg-white border-gray-100 border-b-2 hover:shadow-2xl hover:shadow-yellow-500 hover:bg-amber-100 shadow-md shadow-slate-400 rounded-lg p-6 w-80">
                 <h2 className="text-2xl font-semibold mb-4">Harsh Mahajan</h2>
                 <p className="text-gray-700 mb-4">
                   The project requirements gathering, data collection, and
@@ -119,15 +121,17 @@ const AboutUs = () => {
                     href="https://github.com/harshmahajan"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-slate-800 text-2xl"
+                    className="text-slate-800 text-2xl shadow-lg shadow-gray-600 rounded-lg hover:-translate-y-2 duration-200 transition-all hover:shadow-gray-900 hover:shadow-xl"
                   >
                     <FaGithubAlt />
                   </a>
                   <a
-                    href="mailto:harsh@example.com"
-                    className="text-slate-800 text-2xl"
+                    href="https://www.linkedin.com/in/harsh-mahajan"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-slate-800 text-2xl shadow-lg shadow-gray-600 hover:-translate-y-2 duration-200 transition-all hover:shadow-gray-900 hover:shadow-xl"
                   >
-                    <FaEnvelope />
+                    <FaLinkedin />
                   </a>
                 </div>
               </div>
