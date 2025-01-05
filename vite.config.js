e5b82ddf5dbb4a0569b21b4e25a4d1113badca3d
Rohan More <rohan110620@gmail.com>
2025-01-05 22:20:20 +0530

    commited

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Change this to 'build' if you want the output directory to be named 'build'
+    outDir: "dist",
   },
 });
-// vite.config.js
-/* import reactRefresh from '@vitejs/plugin-react-refresh';
-
-export default {
-  plugins: [reactRefresh()],
-};
- */
